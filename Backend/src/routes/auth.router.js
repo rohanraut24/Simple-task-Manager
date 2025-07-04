148f6ffca801f76b35b0717fbc06a23edcbba708
zaids100 <zaidshaikh2619@gmail.com>
2025-07-04 17:31:15 +0530

    added a comment in router

--- Backend/src/routes/auth.router.js
+++ Backend/src/routes/auth.router.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { signup ,login} from '../controllers/auth.controller.js';
 const router =express.Router();
 
-router.post("/signup",signup);
+router.post("/signup",signup); // this is router for signup 
 router.post("/login",login);
 
 export default router;
